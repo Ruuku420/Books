3108d3dbea83af0232d7e70b614a992562bd86ec
Ruuku420 <ruuku@tuta.io>
2024-02-18 17:01:04 -1000

    Before my computer is fucked

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+# import asyncio
+import books_utils as bk
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def root():
+    return ""
+
+@app.route("/books", methods=['GET', 'POST', 'DEL'])
+def books():
+    match request.method:
+        case 'GET':
+            return bk.get_request(request.args)
+        case 'POST':
+            return bk.post_request(request.args)
+        case 'DEL':
+            return bk.Del(request.args)
+        case _:
+            return "Something went really wrong"
+
+if __name__ == '__main__':
+    app.run()
---
+++ books_utils.py
@@ -0,0 +1,13 @@
+import security
+
+
+def get_request(args):
+    # guard clause
+    required_args = ("title", "author", "token")
+
+
+def post_request(args):
+    pass
+
+def del_request(args):
+    pass
